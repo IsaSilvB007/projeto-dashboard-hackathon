6c5d1df8eb47789002e81bf5560d0d54ef072bca
Wanderson-Valentim <wanderson200108@gmail.com>
2026-02-13 15:12:09 -0300

    feat: definir constantes globais e dados mockados para o dashboard
    - Implementar regionColors e translatedRegions em constants.js
    - Adicionar dados mockados do Rest Countries em mock.js para suporte ao desenvolvimento
    
    [https://app.shortcut.com/hackathonproject/story/53/p%C3%A1gina-inicial-filtros-gr%C3%A1ficos-e-listagem]

---
+++ js/constants.js
@@ -0,0 +1,17 @@
+export const regionColors = [
+  '#FFCE56',
+  '#36A2EB',
+  '#FF6384',
+  '#4BC0C0',
+  '#ff9e66',
+  '#9966FF',
+];
+
+export const translatedRegions = {
+  "africa": "África",
+  "americas": "Américas",
+  "asia": "Ásia",
+  "europe": "Europa",
+  "oceania": "Oceania",
+  "antarctic": "Antártida"
+}
