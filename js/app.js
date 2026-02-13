dbc4b4181801bfcd24875b2caf9f09fb43cb2a39
Wanderson-Valentim <wanderson200108@gmail.com>
2026-02-13 16:41:15 -0300

    feat: implementar orquestrador central e simulação de busca de dados
    - Adicionar app.js com lógica de roteamento e inicialização da aplicação
    - Implementar simulação de atraso (loading) e renderização condicional
    - Atualizar index.html com elementos de feedback visual (spinner) e novo import principal
    
    [https://app.shortcut.com/hackathonproject/story/53/p%C3%A1gina-inicial-filtros-gr%C3%A1ficos-e-listagem]

---
+++ js/app.js
@@ -0,0 +1,44 @@
+import { getCountriesByRegion, getPopulationByRegion, toggleChartLoading } from './helpers.js';
+import { mockData } from './mock.js';
+import { renderCountriesList, renderTableSkeleton } from './ui.js';
+import { sortCountriesList } from './helpers.js';
+import { createMainPageChart } from './chart.js';
+
+const init = async () => {
+  const isHomePage = window.location.pathname.endsWith('index.html') || window.location.pathname === '/';
+  if (!isHomePage) return;
+
+  const tableBody = '#countries-list';
+
+  renderTableSkeleton(tableBody);
+  toggleChartLoading('wrapper-country', true);
+  toggleChartLoading('wrapper-population', true);
+
+  try {
+    // Simula um delay de carregamento para API de países enquanto a integração não é feita
+    await new Promise(resolve => setTimeout(resolve, 2000));
+    const newData = mockData.map(country => ({
+      name: country.translations.por.common,
+      region: country.region.toLowerCase(),
+      population: country.population,
+      flag: country.flags.svg,
+      cca3: country.cca3
+    }));
+    const countries = sortCountriesList(newData);
+
+    renderCountriesList(countries, tableBody);
+
+    const countriesByRegion = getCountriesByRegion(countries);
+    const populationByRegion = getPopulationByRegion(countries);
+
+    createMainPageChart(document.getElementById('countryChart'), 'bar', countriesByRegion);
+    createMainPageChart(document.getElementById('populationChart'), 'pie', populationByRegion);
+
+    toggleChartLoading('wrapper-country', false);
+    toggleChartLoading('wrapper-population', false);
+  } catch (error) {
+    console.error("Erro ao carregar dados:", error);
+  }
+};
+
+init();
