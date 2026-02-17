9a5062bf62ec9ef764aea104e488a58f66dd8438
Wanderson-Valentim <wanderson200108@gmail.com>
2026-02-17 15:47:40 -0300

    feat: implementar utilitários de alerta e processamento de clima
    - Adicionar função errorAlert integrada ao SweetAlert2 para notificações de erro
    - Implementar groupWeatherByDays para processar e agrupar previsões da OpenWeather
    
    [https://app.shortcut.com/hackathonproject/story/89/ajustes-finais-no-projeto]

--- js/helpers.js
+++ js/helpers.js
@@ -44,3 +44,48 @@ export const toggleChartLoading = (wrapperId, isLoading) => {
     canvas.classList.remove('hidden');
   }
 };
+
+export const errorAlert = (text) => {
+  Swal.fire({
+    title: 'Erro ao carregar dados!',
+    text: text,
+    icon: 'error',
+    showCancelButton: true,
+    confirmButtonColor: '#3085d6',
+    cancelButtonColor: '#d33',
+    confirmButtonText: 'Sim, recarregar página',
+    cancelButtonText: 'Sair'
+  }).then((result) => {
+    if (result.isConfirmed) {
+      window.location.reload();
+    }
+  });
+}
+
+export const groupWeatherByDays = (forecastData) => {
+  const grouped = forecastData.list.reduce((acc, item) => {
+    const today = new Date().toISOString().split('T')[0];
+    const dateStr = item.dt_txt.split(' ')[0];
+
+    if (dateStr !== today) {
+      
+      const dayName = new Date(item.dt_txt).toLocaleDateString('pt-BR', { weekday: 'short' });
+      
+      if (!acc[dayName]) {
+        acc[dayName] = { temps: [], total: 0 };
+      }
+
+      acc[dayName].temps.push(item.main.temp);
+    }
+    return acc;
+  }, {});
+
+  const result = {};
+  for (const day in grouped) {
+    const sum = grouped[day].temps.reduce((a, b) => a + b, 0);
+    const average = sum / grouped[day].temps.length;
+    result[day] = Math.round(average * 10) / 10;
+  }
+
+  return result;
+};
