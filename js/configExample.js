c64c8bf24419b81568fe2b0458c6a24f2db82c72
Wanderson-Valentim <wanderson200108@gmail.com>
2026-02-17 15:48:38 -0300

    feat: implementar serviço de API e exemplo de configuração
    - Criar countryService com métodos getAll e getDetails para chamadas de API
    - Adicionar configExample.js para exemplificar a estrutura necessária do config.js
    
    [https://app.shortcut.com/hackathonproject/story/89/ajustes-finais-no-projeto]

---
+++ js/configExample.js
@@ -0,0 +1,5 @@
+export const CONFIG = {
+  WEATHER_API_KEY: 'YOUR_API_KEY_HERE',
+  COUNTRIES_BASE_URL: 'https://restcountries.com/v3.1',
+  WEATHER_BASE_URL: 'https://api.openweathermap.org/data/2.5',
+}
