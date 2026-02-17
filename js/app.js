024b7a12370dd1954f2d397b0376f891eedaf8e5
Wanderson-Valentim <wanderson200108@gmail.com>
2026-02-17 15:51:29 -0300

    feat: orquestrar inicialização de rotas e integrar eventos ao service
    - Organizar lógica de inicialização para páginas Home e Detalhes no app.js
    - Atualizar events.js para realizar buscas de dados através do countryService
    
    [https://app.shortcut.com/hackathonproject/story/89/ajustes-finais-no-projeto]

--- js/app.js
+++ js/app.js
@@ -1,114 +1,56 @@
 import { getCountriesByRegion, getPopulationByRegion, toggleChartLoading } from './helpers.js';
-//import { mockData } from './mock.js';
-import { renderCountriesList, renderTableSkeleton } from './ui.js';
-import { sortCountriesList } from './helpers.js';
-import { createMainPageChart } from './chart.js';
-import { CONFIG } from './config.js';
+import { renderCountriesList, renderCountryDetails, renderTableSkeleton, renderWeatherList } from './ui.js';
+import { createMainPageChart, renderWeatherChart } from './chart.js';
+import { countryService } from './service.js';
 
-export const countryService={
-
-  getAll: async()=>{
-    try {
-      const res = await fetch('https://restcountries.com/v3.1/all?fields=name,translations,region,population,flags,cca3');
-
-      if (!res.ok) {
-        throw new Error(`Erro na API: ${res.status} - ${res.statusText}`);
-      }
-      const data = await res.json();
-      return data;
-
-      }catch (error) {
-      console.error("Falha ao buscar lista de países:", error.message);
-      return { erro: true, mensagem: error.message };
-    }
-  },
-
-  getDetails: async(nomePais)=>{
-    
-    const apiClimaKey = CONFIG.API_CLIMA_KEY;
-
-  if(!nomePais) return {erro: "Nenhum nome fornecido"}
-
-  try{
-      const resPais = await fetch(`https://restcountries.com/v3.1/name/${nomePais}`);
-      if(!resPais.ok) throw new Error("País não encontrado.");
-      const objPais = await resPais.json();
-      const p=objPais[0];
-      console.log(p);
-
-      const capital = p.capital ? p.capital[0] : null;
-      let dadosClima = {temperatura: "" , descricao: "" }
-      if(capital){
-        const resClima = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${capital}&appid=${apiClimaKey}&units=metric&lang=pt_br`);
-        if(resClima.ok){
-        const objClima = await resClima.json();
-        dadosClima={
-          temperatura: Math.round(objClima.main.temp)+"°C",
-          descricao: objClima.weather[0].description
-        };
-        console.log(objClima);
-      }
-    }
-      const objDados={
-        nome: p.translations?.por?.common || p.name.common,
-        nomeNativo: p.name.nativeName ? Object.values(p.name.nativeName)[0].common : p.name.common,
-        bandeira: p.flags.svg,
-        capital: capital || "",
-        populacao: p.population.toLocaleString('pt-BR'),
-        regiao: p.region,
-        subRegiao: p.subregion,
-        moeda: p.currencies ? Object.values(p.currencies).map(m => m.name).join(", ") : "N/A",
-        idiomas: p.languages ? Object.values(p.languages).join(", ") : "N/A",
-        clima: dadosClima,
-        fronteiras: p.borders || [],   
-      };
-
-      sessionStorage.setItem("dadosFinais", JSON.stringify(objDados));
-      return objDados;
-      
-    }catch(error){
-      return { erro: error.message };
-    }
-  },
-}
-  
 
 const init = async () => {
   const isHomePage = window.location.pathname.endsWith('index.html') || window.location.pathname === '/';
-  if (!isHomePage) return;
+  const isDetailPage = window.location.pathname.endsWith('detalhes.html');
 
-  const tableBody = '#countries-list';
+  if (isHomePage) {
+    const tableBody = '#countries-list';
+    renderTableSkeleton(tableBody);
+    toggleChartLoading('wrapper-country', true);
+    toggleChartLoading('wrapper-population', true);
+  
+    try {
+      const countries = await countryService.getAll();
+      if(countries === null) throw new Error("Falha ao carregar dados dos países.");
+  
+      renderCountriesList(countries, tableBody);
+  
+      const countriesByRegion = getCountriesByRegion(countries);
+      const populationByRegion = getPopulationByRegion(countries);
+  
+      createMainPageChart(document.getElementById('countryChart'), 'bar', countriesByRegion, 'Quantidade de Países por Região');
+      createMainPageChart(document.getElementById('populationChart'), 'pie', populationByRegion, 'População por Região');
+  
+      toggleChartLoading('wrapper-country', false);
+      toggleChartLoading('wrapper-population', false);
+    } catch (error) {
+      console.error("Erro ao carregar dados:", error);
+    }
+  }
 
-  renderTableSkeleton(tableBody);
-  toggleChartLoading('wrapper-country', true);
-  toggleChartLoading('wrapper-population', true);
+  if (isDetailPage) {
+    try {
+      const queryString = window.location.search;
+      const urlParams = new URLSearchParams(queryString);
+      const country = await countryService.getDetails(urlParams.get('code'));
+      
+      renderCountryDetails(country, '#country-details');
+      renderWeatherList(country, '#country-details');
 
-  try {
-
-    //await new Promise(resolve => setTimeout(resolve, 2000));
-    const data = await countryService.getAll();
-    if(data.erro) throw new Error(data.erro);
-
-    const countries = sortCountriesList(data.map(country => ({
-      name: country.translations?.por?.common || country.name.common,
-      region: country.region.toLowerCase(),
-      population: country.population,
-      flag: country.flags.svg,
-      cca3: country.cca3
-    })));
-
-    renderCountriesList(countries, tableBody);
-
-    const countriesByRegion = getCountriesByRegion(countries);
-    const populationByRegion = getPopulationByRegion(countries);
-
-    createMainPageChart(document.getElementById('countryChart'), 'bar', countriesByRegion, 'Quantidade de Países por Região');
-    createMainPageChart(document.getElementById('populationChart'), 'pie', populationByRegion, 'População por Região');
-
-    toggleChartLoading('wrapper-country', false);
-    toggleChartLoading('wrapper-population', false);
-  } catch (error) {
-    console.error("Erro ao carregar dados:", error);
+      country.weather.forEach(weather => {
+        const element = document.getElementById(`forecast-chart-${weather.city.toLowerCase().trim()}`);
+        if (element) {
+          renderWeatherChart(element, weather.forecast);
+        }
+      })
+    } catch (error) {
+      console.error("Erro ao carregar dados:", error);
+    }
   }
 };
 
--- js/events.js
+++ js/events.js
@@ -1,34 +1,39 @@
-import { mockData } from './mock.js';
 import { renderCountriesList } from './ui.js';
-import { filterCountries, sortCountriesList } from './helpers.js';
-
-// Essa parte é temporária e simula os dados recuperados na session storage
-const newData = mockData.map(country => ({
-  name: country.translations.por.common,
-  region: country.region.toLowerCase(),
-  population: country.population,
-  flag: country.flags.svg,
-  cca3: country.cca3
-}));
-const countries = sortCountriesList(newData);
+import { filterCountries,  } from './helpers.js';
+import { countryService } from './service.js';
 
 const searchInput = document.querySelector('#search-input');
 const regionSelect = document.querySelector('#region-filter');
 
-searchInput.addEventListener('input', (event) => {
-  const inputValue = event.target.value.toLowerCase().trim();
-  const selectValue = regionSelect.value;
- 
-  const filteredCountries = filterCountries(inputValue, selectValue, countries);
-  
-  renderCountriesList(filteredCountries, '#countries-list');
+searchInput.addEventListener('input', async (event) => {
+  try {
+    const countries = await countryService.getAll();
+    if(countries === null) throw new Error("Falha ao carregar dados dos países.");
+
+    const inputValue = event.target.value.toLowerCase().trim();
+    const selectValue = regionSelect.value;
+   
+    const filteredCountries = filterCountries(inputValue, selectValue, countries);
+    
+    renderCountriesList(filteredCountries, '#countries-list');
+    
+  } catch (error) {
+    console.error("Erro ao carregar dados:", error);
+  }
 });
 
-regionSelect.addEventListener('change', (event) => {
-  const selectValue = event.target.value.toLowerCase().trim();
-  const inputValue = searchInput.value.toLowerCase().trim();
- 
-  const filteredCountries = filterCountries(inputValue, selectValue, countries);
+regionSelect.addEventListener('change', async (event) => {
+  try {
+    const countries = await countryService.getAll();
+    if(countries === null) throw new Error("Falha ao carregar dados dos países.");
 
-  renderCountriesList(filteredCountries, '#countries-list');
+    const selectValue = event.target.value.toLowerCase().trim();
+    const inputValue = searchInput.value.toLowerCase().trim();
+    
+    const filteredCountries = filterCountries(inputValue, selectValue, countries);
+  
+    renderCountriesList(filteredCountries, '#countries-list');
+  } catch (error) {
+    console.error("Erro ao carregar dados:", error);
+  }
 });
